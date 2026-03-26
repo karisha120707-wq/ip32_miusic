0063569fd46d640d03053cabb8090f1a8400fb7d
Ksusha and Dolgor <tsydenovadolgor08@gmail.com>
2026-03-26 12:43:14 +0700

    Добавлена модель исполнителя

--- main/models.py
+++ main/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+class Artist (models.Model):
+    name = models.CharField(max_length=500, unique=True)
+    image = models.ImageField(upload_to='artists/', null=True)
+
+    def __str__(self):
+        return self.name
+
+
 
 class Genre(models.Model):
     name_ru = models.CharField(max_length=500)
@@ -14,5 +22,6 @@ class Tracks(models.Model):
     title = models.CharField(max_length=500)
     duration = models.IntegerField(max_length=500)
     genre =  models.ManyToManyField(Genre)
+    artists =models.ForeignKey(Artist, on_delete=models.RESTRICT, null=True)
     def __str__(self):
         return self.title
